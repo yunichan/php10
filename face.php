259196d99ef1b6dc2913b5235e70643695903eb3
yuni <yuni.tonoseaki@gmial.com>
2018-02-15 01:33:07 +0900

    commit

--- face.php
+++ face.php
@@ -7,12 +7,8 @@
     //$referer = "各自設定してください";
 
     //画像へのパス
-    $image_path = "./test.jpg";
-	//test
-	$img_data = base64_encode(file_get_contents($image_path));
-	echo('<pre>');
-	var_dump($img_data);
-	echo('</pre>');
+    $image_path = "image/nana1.jpg";
+
 	
     //リクエスト用のJSON生成
     $json = json_encode(array(
@@ -30,48 +26,20 @@
             ),
         ),
     ));
-	echo('<pre>');
-	var_dump($json);
-	echo('</pre>');
+
 	
     //APIアクセス
     $curl = curl_init();
-    $a=curl_setopt($curl, CURLOPT_URL, "https://vision.googleapis.com/v1/images:annotate?key=" . $api_key);
-	echo('<pre>');
-	var_dump($a);
-	echo('</pre>');
-    $b=curl_setopt($curl, CURLOPT_HEADER, true); 
-	echo('<pre>');
-	var_dump($b);
-	echo('</pre>');
-    $c=curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-	echo('<pre>');
-	var_dump($c);
-	echo('</pre>');
-    $d=curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-	echo('<pre>');
-	var_dump($d);
-	echo('</pre>');
-    $e=curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	echo('<pre>');
-	var_dump($e);
-	echo('</pre>');
-    $f=curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	echo('<pre>');
-	var_dump($f);
-	echo('</pre>');
-    $g=curl_setopt($curl, CURLOPT_TIMEOUT, 15);
-	echo('<pre>');
-	var_dump($g);
-	echo('</pre>');
-    $h=curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-	echo('<pre>');
-	var_dump($h);
-	echo('</pre>');
+    curl_setopt($curl, CURLOPT_URL, "https://vision.googleapis.com/v1/images:annotate?key=" . $api_key);
+    curl_setopt($curl, CURLOPT_HEADER, true); 
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 15);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
     $res1 = curl_exec($curl);
-	echo 'curl_exec check';
-	echo curl_error($res1);
-	$res2=curl_getinfo($curl);
+	$res2 = curl_getinfo($curl);
     curl_close($curl);
 
     //データ取得
@@ -80,7 +48,6 @@
 
     //データ確認
     $arr = json_decode($json,true);
-	var_dump($arr);
 
 ?>
 
@@ -91,8 +58,8 @@
 
 <style>
 .point {
-    width: 6px;
-    height: 6px;
+    width: 3px;
+    height: 3px;
     display: block;
     position: absolute;
     background-color: red;
@@ -102,15 +69,17 @@
 </head>
 
 <body>
-
-<div style="background-image:url(./test.jpg); position: absolute; width:356px; height: 380px;">
+<div style="background-image:url(image/nana1.jpg); position: absolute; width:500px; height:100%; background-size: 500px; background-repeat: no-repeat;">
 <a href=""></a>
+
+
 <?php
+// print_r($arr["responses"][0]["faceAnnotations"][0]["fdBoundingPoly"]["vertices"][0]["x"]);
     foreach ($arr["responses"][0]["faceAnnotations"][0]["landmarks"] as $key => $value) {
-        $x = $value["position"][y]-3;
-        $y = $value["position"][x]-3;
+        $x = $value["position"]["y"];
+        $y = $value["position"]["x"];
         echo <<<EOF
-<a href="" class="point" style="top:{$x}px; left: {$y}px;"></a>
+    <a href="" class="point" style="top:{$x}px; left: {$y}px;"></a>
 EOF;
     }
 ?>
