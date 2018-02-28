5e9f3998e736923d2a6c1fc066fcd64dd3032d8a
yuni <yuni.tonoseaki@gmial.com>
2018-03-01 00:40:05 +0900

    add

--- output.php
+++ output.php
@@ -86,20 +86,5 @@ if($return_var === 0) {
 ?>
 
 </div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>
-$(function() {
-  var h = $(window).height();
- 
-  $('#wrap').css('display','none');
-  $('#loader-bg ,#loader').height(h).css('display','block');
-});
- 
-$(window).load(function () { //全ての読み込みが完了したら実行
-  $('#loader-bg').delay(900).fadeOut(800);
-  $('#loader').delay(600).fadeOut(300);
-  $('#wrap').css('display', 'block');
-});
-</script>
 </body>
 </html>
