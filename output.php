577ecedfa84d2e535f109cacfb351ad3307d7d3a
yuni <yuni.tonosaki@gmail.com>
2018-03-02 14:13:54 +0900

    Update output.php

--- output.php
+++ output.php
@@ -15,29 +15,6 @@
     form{
         border: none;
     }
-    #loader-bg {
-        display: none;
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        top: 0px;
-        left: 0px;
-        background: #000;
-        z-index: 1;
-      }
-      #loader {
-        display: none;
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        width: 200px;
-        height: 200px;
-        margin-top: -100px;
-        margin-left: -100px;
-        text-align: center;
-        color: #fff;
-        z-index: 2;
-      }
 </style>
 <body>
 <div id="wrap">
@@ -64,7 +41,7 @@ fputs($fpage,$page);
 fclose($fpage);
 
 /* python 実行 */
-exec("/Users/yuni/anaconda/bin/python3.6 /Applications/XAMPP/xamppfiles/htdocs/gsac/php10/work/google_api.py 2>&1", $output, $return_var) ;
+exec("/Users/yuni/anaconda/bin/python3.6 ./google_api.py 2>&1", $output, $return_var) ;
 if($return_var === 0) {
     foreach($output as $key => $val) {
         echo "return_var : ".$return_var."<br />" ;
@@ -76,7 +53,7 @@ if($return_var === 0) {
         $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
         $obj = json_decode($json,true);
         foreach($obj as $key=>$val){
-            echo "<img src='".$key."'>";
+            echo "<img src='".$key."' width=200>";
         }
 } else {
     echo "exec error : ".$return_var."<br />" ;
